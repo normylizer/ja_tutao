a7d9f68d1916b11ecdfb0a0728dcd961ccda9db2
Skip <114914902+thewolfofeco@users.noreply.github.com>
2025-11-13 09:54:49 +0100

    0.1.0: - Vollständige Überarbeitung des Frontend - Kapselung von Methoden, vgl. Komponenten-Diagramm - Weitere Abbruchbedingungen für den Remote Request hinzugefügt

--- url_check.js
+++ url_check.js
@@ -1,97 +1,226 @@
-////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Thema: Inline URL Überprüfung in JavaScript
-// Datum: 2025-11-04
+// Datum: 2025-11-12
 //
-// Version: 0.0.4 [PoC]
+// Version: 0.1.0 [indev]
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Init
 
-// Konstanten
-const checkDelay = 5000; // ms  -> Proof of Concept: 5000 (5 Sekunden) | Produktiv: 15 (400 Anschläge/Minute)
-const urlRegEx = /(^$|((http(s))?:\/\/)([\w-]+\.)+[\w-]+([\w- ;,.\/?%&=]*))/  // Quelle: https://regex101.com/r/kM8eW3/1
+// Einstellungen
+const settings = {
+	urlRegEx: /(^$|((http(s))?:\/\/)([\w-]+\.)+[\w-]+([\w- ;,.\/?%&=]*))/,  // Quelle: https://regex101.com/r/kM8eW3/1
+	checkDelay: 2000,  // ms  -> Proof of Concept: 2000 (2 Sekunden) | Produktiv: 15 (400 Anschläge/Minute)
+	maxInputLength: 2048,  // http://www.sitemaps.org/protocol.html
 
-// Variablen
-let isValidURL = false; // Global: Kein Server-Request, wenn Eingabe keine URL ist!
-let pendingRequest = false;
-let lastCall = 0;
-
-
-// Webseite vorbereiten: $(document).ready ohne jQuery
-document.addEventListener("DOMContentLoaded", () => {
-	document.getElementById("input_btn").setAttribute("disabled", "disabled");
-});
-
-// URL Validierung
-function validateURL() {
-	// Werte werden bei jeder Eingabe zurückgesetzt
-	let x = document.getElementById("input_url").value;
-	let validateText = "Keine Eingabe";
-	isValidURL = false;
-	document.getElementById("input_btn").setAttribute("disabled", "disabled");
-
-	if (x !== "") {
-		if (x.match(urlRegEx)) {
-			// Eingabe entspricht RegEx
-			document.getElementById("validator_result").setAttribute("style", "color: green;"); // Visuelles Feedback
-            document.getElementById("input_btn").removeAttribute("disabled");
-            validateText = "Valide URL: " + x;
-			isValidURL = true;
-			remoteCheckURL();
-	    } else {
-			// Eingabe entspricht nicht RegEx
-			document.getElementById("validator_result").setAttribute("style", "color: red;"); // Visuelles Feedback
-            validateText = "Keine valide URL: " + x;
-			isValidURL = false;
-	    }
-	}
-    document.getElementById("validator_result").innerHTML = validateText;
+	// Simulierte Antworten des Remote Servers
+	//  Nur responseText, readyState == 4 und status == 200 wird unterstellt.
+	//  Es ist ein guter und zuverlässiger simulierter Remote Server.
+	remoteServer: "remote://irgendwas.tld", // Wird aktuell nur in einem Kommentar referiert
+	serverResponseText: [
+		"Diese URL ist nicht bekannt.",
+		"Diese URL ist bekannt und verweist auf eine Datei.",
+		"Diese URL ist bekannt und verweist auf einen Ordner."
+	]
 }
 
-// Check Request
-function remoteCheckURL() {
-	let currentCall = Date.now();
-	let passedDelay = currentCall - lastCall;
-	
-	// Abbruchbedingung: Es wurde bereits ein Request ausgelöst
-	if (pendingRequest) { return; }
-	
-	// Neuen Request auslösen
-	if (passedDelay <= checkDelay) {
-		// Es ist bereits ein Request ausgelöst
-		pendingRequest = true; // Parallele Ausführung sperren
-	    setTimeout(ajaxRequest, checkDelay - passedDelay);
-		pendingRequest = false; // Sperre aufheben
 
-		// Rückmeldung zur Visualisierung, später entfernen oder an sinnvolleren Ort verschieben
-		document.getElementById("remote_check_result").innerHTML = "Der nächste Request ist ausgelöst und erfolgt in  " + (checkDelay - passedDelay) + " ms, bitte warten.";
-	} else {
-		// Es ist noch kein Request ausgelöst
-		ajaxRequest();
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Business-Logik
+
+const InputLogic = {
+	// Clientseitige Validierung
+	// Kriterien: - input darf nicht leer sein
+	//            - input darf nicht länger als in den Settings angegeben sein
+	//            - input muss dem angegebenen RegEx entsprechen
+	isNotEmpty: function(input) {
+		ErrorHandler.Report(2,"Leere Eingabe");
+		return (input);
+	},
+	isValidLength: function(input) {
+		ErrorHandler.Report(2,"Eingabe zu lang. Es sind maximal " + settings.maxInputLength + " Zeichen erlaubt.");
+		// OFI: Eingabe technisch verkürzen?
+		return (input.length <= settings.maxInputLength);
+	},
+	isValidURL: function(input) {
+		ErrorHandler.Report(2,"Das ist keine gültige URL.");
+		const urlRegEx = settings.urlRegEx;
+		return input.match(urlRegEx);
+	},
+	isValid(input) {
+		if (this.isNotEmpty(input) && this.isValidLength(input) && this.isValidURL(input)) {
+			ErrorHandler.Report(1,"Gültige Eingabe!")
+			return true;
+		}
+	},
+
+	// Gated Getter
+	GetUserInput() {
+		// Rudimentäres htmlspecialchars(), um Code Injection zu verhindern
+		let escapeMap = {
+			'&': '&amp;',
+			'<': '&lt;',
+			'>': '&gt;',
+			'"': '&quot;',
+			"'": '&#039;'
+		}
+		return FrontendConnector.GetUserInput().replace(/[&<>"']/g, function(m) { return map[m]; });
+	},
+
+	// Gated Check
+	Check(userInput) {
+		// Der RemoteConnector verwaltet die Anfragen selbst. Er übergibt bei Antwort an Report().
+		RemoteConnector.Check(userInput);
+	},
+
+	// Response handling
+	Report(checkResult) {
+		FrontendConnector.SetCheckResult(checkResult);
 	}
-	
-    lastCall = currentCall; // Hebelt auf Grund der Abbruchbedingung nicht den Throttle aus
 }
 
-// Der eigentliche Remote call
-function ajaxRequest() {
-
-	// Abbruchbedingung: Eingabe ist keine valide URL (mehr)
-	if (isValidURL == false) {
-		document.getElementById("remote_check_result").innerHTML = "Request abgebrochen. Es ist keine valide URL (mehr) eingetragen."
-		return;
+// Steuerung der Rückmeldungen an den Nutzer
+// Roadmap UX: Verzögerung in die Rückmeldung für eine sanftere UX
+const ErrorHandler = {
+	Report: function(errorLevel,errorMessage){
+		FrontendConnector.SetErrorLevel(errorLevel);
+		FrontendConnector.SetContextInfo(errorMessage);
+	},
+	Clear: function(){
+		FrontendConnector.ClearErrorLevel();
+		FrontendConnector.ClearContextInfo();
 	}
+}
 
-    // So könnte ein AJAX-Request aussehen:
-    /**********************************************
-	const remoteServer = "remote://irgendwas.tld";
-    let xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-		document.getElementById("remote_check_result").innerHTML = xhttp.responseText;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Konnektoren
+
+// Konnektor zur standardisierten Interaktion mit dem Frontend. Alle Interaktionen mit dem DOM erfolgen hier.
+// Erfüllt Aufgabe 1: Clientseitige Validierung
+const FrontendConnector = {
+	// Setter
+	SetErrorLevel: function(errorLevel){
+		switch(errorLevel) {
+			case 1:
+				document.getElementById("lookup_input").className = "input_good";
+				break;
+			case 2:
+				document.getElementById("lookup_input").className = "input_faulty";
+				break;
+			case 0:
+			default:
+				document.getElementById("lookup_input").className = "input_indifferent";
+		}
+	},
+	SetContextInfo: function(message){
+		document.getElementById("context_info").textContent = message;
+	},
+	SetCheckResult: function(message){
+		document.getElementById("check_result").textContent = message;
+	},
+
+	// Getter
+	GetUserInput: function(){
+		return document.getElementById("input_url").value;
+	},
+
+	// Wiper
+	ClearErrorLevel: function(){
+		this.SetErrorLevel(0);
+	},
+	ClearContextInfo: function(){
+		this.SetContextInfo("");
 	}
-	xhttp.open("GET", remoteServer, true);
-	xhttp.send();
-    ***********************************************/
+}
 
-	// Was tatsächlich geschah
-	document.getElementById("remote_check_result").innerHTML = "Theoretische Antwort. (" + Date() + ")";
+// Konnektor zur standardisierten Interaktion mit dem Remote-Server.
+// "Echte" Klasse, um statische Variablen nutzen zu können.
+class RemoteConnectorEnv {
+	static requestString = "";
+	static lastString = "";
+	static lastRequest = 0;
+	static pendingRequest = false;
 
+	constructor(){}
+
+	// Setter
+	SetRequestString(userInput){ this.requestString = userInput; }
+	SetLastString(requestString){ this.lastString = requestString; }
+	SetLastRequest(timeStamp){ this.lastRequest = timeStamp; }
+	SetPendingRequest(isPending){ this.pendingRequest = isPending;}
+
+	// Getter
+	GetRequestString(){ return this.requestString;}
+	GetLastString(){ return this.lastString;}
+	GetLastRequest(){ return this.lastRequest;}
+	hasPendingRequest(){ return this.pendingRequest;}
+
+	// Der eigentliche Request
+	ServerRequest(){
+	    // So könnte ein AJAX-Request aussehen:
+	    /**********************************************
+	    let xhttp = new XMLHttpRequest();
+		// ToDo: Payload this.requestString vorbereiten
+	    xhttp.onload = function() {
+			InputLogic.Report(responseText);
+		}
+		xhttp.open("GET", settings.remoteServer, true);
+		xhttp.send();
+    	***********************************************/
+
+
+		// Was tatsächlich geschah
+		let antwortNummer = Math.floor(Math.random() * settings.serverResponseText.length);
+		let responseText = settings.serverResponseText[antwortNummer];
+
+		// In jedem Fall gibt es eine Antwort
+		RemoteConnector.SetLastRequest(Date.now());
+		RemoteConnector.SetLastString(RemoteConnector.GetRequestString());
+		RemoteConnector.SetPendingRequest(false);
+		return responseText;
+	}
+	Check(userInput){
+		let currentRequest = Date.now();
+		let currentDelay = currentRequest - this.lastRequest;
+
+		// Lemma: userInput ist bei Aufruf von Check() validiert
+		// Der alte userInput wird verworfen, er ist nicht mehr interessant
+		RemoteConnector.SetRequestString(userInput);
+
+		// Ausführung abbrechen, wenn bereits ein Request angefordert ist
+		if (RemoteConnector.hasPendingRequest()) {	return;	}
+
+		// Ausführung abbrechen, wenn dieselbe URL zuletzt gesucht wurde
+		if (RemoteConnector.GetRequestString() == RemoteConnector.GetLastString()) { return; }
+
+		// Throttled Request
+		this.SetPendingRequest(true);
+		if (currentDelay < settings.checkDelay) {
+			setTimeout(function() {
+				let serverReply = RemoteConnector.ServerRequest();
+				InputLogic.Report(RemoteConnector.GetRequestString() + ": " + serverReply);
+			}, settings.checkDelay);
+		} else {
+			let serverReply = RemoteConnector.ServerRequest();
+			InputLogic.Report(RemoteConnector.GetRequestString() + ": " + serverReply);
+		}
+	}
+}
+const RemoteConnector = new RemoteConnectorEnv();
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Entry Point
+
+// Input Entry Point: InputHandler()
+function InputHandler() {
+	// Vorarbeit: Bereinigte Benutzereingabe aus dem Frontend holen
+	let userInput = InputLogic.GetUserInput();
+
+	// Aufgabe 1: Clientseitige Validitätsprüfung
+	if (InputLogic.isValid(userInput)) {
+
+		// Aufgabe 2: Remote request
+		InputLogic.Check(userInput);
+	};
 }
